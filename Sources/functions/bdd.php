cbc3596131adae80181578e365527a734b679842
camus.charlie@gmail.com <camus.charlie@gmail.com@81e71bab-4a8d-29b4-85ac-da22c4f186d2>
2012-10-19 14:00:11 +0000

    Modification de la gestion de la bdd, tout cassé ! ajout du test de la BDD, test passé OK

--- Sources/functions/bdd.php
+++ Sources/functions/bdd.php
@@ -1,11 +1,51 @@
 <?php
-	function getBDD(){
-		include('InstallInfo.php');
-		mysql_connect($bddPlace, $bddUser, $bddMdp) or die("Erreur de connection");
-		mysql_select_db($nomBDD) or die ("Erreur de connection à la base de données");
+	// function getBDD(){
+		// include('InstallInfo.php');
+		// mysql_connect($bddPlace, $bddUser, $bddMdp) or die("Erreur de connection");
+		// mysql_select_db($nomBDD) or die ("Erreur de connection à la base de données");
+	// }
+	
+	// function deco(){
+		// mysql_close();
+	// }
+	
+	class BddConnector{
+		private $host;
+		private $database;
+		private $user;
+		private $password;
+		private $connexion;
+		
+		public function __construct(){
+			include('InstallInfo.php');
+			$this->host=$bddPlace;
+			$this->database=$nomBDD;
+			$this->user=$bddUser;
+			$this->password=$bddMdp;
+		}
+		  
+		private function connexion() {
+			try {
+				$this->connexion=new PDO('mysql:host='.$this->host.';dbname='.$this->database,$this->user,$this->password);
+				$this->connexion->setattribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+			}
+			catch(PDOException $e)
+			{
+				echo "La base de donnees est deconnectee. Veuillez reessayer de charger la page.";
+			}
+		}
+		
+		public function getConnexion(){
+			if($this->connexion==null){
+				$this->connexion();
+			}
+			return $this->connexion;
+		}
+
+		//Fonction pour la déconnexion
+		public function deconnexion() {
+			$this->connexion=null;
+		}
 	}
 	
-	function deco(){
-		mysql_close();
-	}
 ?>
