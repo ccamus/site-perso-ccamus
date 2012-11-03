c5f85d2695b7f6b039010d06669b7131ccd6e57d
camus.charlie@gmail.com <camus.charlie@gmail.com@81e71bab-4a8d-29b4-85ac-da22c4f186d2>
2012-11-03 14:58:49 +0000

    Blog a peu près fonctionnel, beug sur le précédent et le suivant, voir les requêtes dans BlogReader.php

---
+++ trunk/Sources/deleteArticle.php
@@ -0,0 +1,36 @@
+<?php
+
+	session_start();
+	include("functions.php");
+	if(isset($_SESSION['userName']) && isset($_SESSION['pwd'])){
+		if(isExist($_SESSION['userName'],$_SESSION['pwd'])){
+			//il existe et est authorisé
+			enTete();
+			echo '<form class="form-horizontal" method="post" action="DeleteArticleInBase.php">
+						<div class="controls">
+							<legend>Suppression d\'article</legend>
+						</div>
+						
+						
+						<div class="control-group">
+							<label class="control-label" for="articles">Titre de l\'article :</label>
+							<div class="controls">
+								'.listArticlesForModify().'
+							</div>
+						</div>
+						
+						
+						<div class="control-group">
+							<div class="controls">
+								<button type="submit" class="btn">Supprimer !</button>
+							</div>
+						</div>
+					</form>';
+		}else{
+			redirAccueil("9");
+		}
+	}else{
+		redirAccueil("9");	
+	}	
+	basPage();
+?>
