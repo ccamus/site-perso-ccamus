9c0e34c63b08847d6e9cc4d2ce99ff682085da21
camus.charlie@gmail.com <camus.charlie@gmail.com@81e71bab-4a8d-29b4-85ac-da22c4f186d2>
2012-11-07 15:06:46 +0000

    Terminé ! Reste à faire le lien avec les api de communication FB et twitter

---
+++ trunk/Sources/deleteComm.php
@@ -0,0 +1,23 @@
+<?php
+
+	session_start();
+	include("functions.php");
+	if(isset($_SESSION['userName']) && isset($_SESSION['pwd'])){
+		if(isExist($_SESSION['userName'],$_SESSION['pwd'])){
+			if(isset($_GET['com']) && isset($_GET['art'])){
+				$commentaire=new Commentaire();
+				$commentaire->setIdCommentaire($_GET['com']);
+				
+				$rep=$commentaire->delete();
+				
+				redirVisuArticle($_GET['art'],$rep);	
+			}else{
+				redirAccueil("13");	
+			}
+		}else{
+			redirAccueil("9");	
+		}
+	}else{
+		redirAccueil("9");	
+	}	
+?>
