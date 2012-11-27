1a132ebb2bf8f6eb0ca28c280e36be1d883b0d3f
camus.charlie@gmail.com <camus.charlie@gmail.com@81e71bab-4a8d-29b4-85ac-da22c4f186d2>
2012-11-27 09:18:21 +0000

    ajout de l'activation ou non des commentaires

--- trunk/Sources/visuArticle.php
+++ trunk/Sources/visuArticle.php
@@ -21,89 +21,91 @@
 			echo'</div><br/><section id="com"><br/>';
 			
 			//affichage des commentaires
-			$commentaires=$article->getCommentaires();
-			$isAdmin=false;
-			if(isset($_SESSION['userName']) && isset($_SESSION['pwd']) && isExist($_SESSION['userName'],$_SESSION['pwd'])){
-				$isAdmin=true;
-			}
-			foreach($commentaires as $commentaire){
-				echo '<div class="row"><div class="span1"></div>';
-				echo '<div class="span10"><div class="well well-small">';
-				
-				echo '<i class="icon-user"></i> <small>'.$commentaire->getCommentateur().'</small>';
-				echo '<p class="pull-right"><i class="icon-calendar"></i> <small>'.$commentaire->getDateComm().'</small></p><br/>';
-				echo $commentaire->getCommentaire();
-				if($isAdmin){
-					//C'est un admin ?
-					echo '<p class="pull-right"><small><a href="deleteComm.php?com='.$commentaire->getIdCommentaire().'&art='.$_GET['art'].'">Supprimer</a></small></p>';
+			if($commActifs){
+				$commentaires=$article->getCommentaires();
+				$isAdmin=false;
+				if(isset($_SESSION['userName']) && isset($_SESSION['pwd']) && isExist($_SESSION['userName'],$_SESSION['pwd'])){
+					$isAdmin=true;
+				}
+				foreach($commentaires as $commentaire){
+					echo '<div class="row"><div class="span1"></div>';
+					echo '<div class="span10"><div class="well well-small">';
+					
+					echo '<i class="icon-user"></i> <small>'.$commentaire->getCommentateur().'</small>';
+					echo '<p class="pull-right"><i class="icon-calendar"></i> <small>'.$commentaire->getDateComm().'</small></p><br/>';
+					echo $commentaire->getCommentaire();
+					if($isAdmin){
+						//C'est un admin ?
+						echo '<p class="pull-right"><small><a href="deleteComm.php?com='.$commentaire->getIdCommentaire().'&art='.$_GET['art'].'">Supprimer</a></small></p>';
+					}
+					
+					echo '</div></div>';
+					echo '<div class="span1"></div></div></section>
+						';
 				}
 				
-				echo '</div></div>';
-				echo '<div class="span1"></div></div></section>
-					';
-			}
-			
-			echo '</section>';
-			
-			//partie insertion de commentaire
-			echo '<section id="addCom"><br/><div class="row"><div class="span2"></div>';
-			echo '<div class="span8"><div class="well well-small">';
-			echo '<i class="icon-comment"></i> <small>Ajouter un commentaire</small>';
-			
-			echo '<form class="form-horizontal" method="post" action="addComment.php?art='.$_GET['art'].'">';
-			echo '<div class="control-group">
-					<label class="control-label" for="commentateur">Votre nom :</label>
-					<div class="controls">
-						<input type="text" name="commentateur" id="commentateur" placeholder="Votre nom" value="';
-			//si il y a un nom en session on prends
-			if(isset($_SESSION['nomCommentateur'])){
-				echo $_SESSION['nomCommentateur'];
-			}
-			echo		'">
-					</div>
-				</div>';
-						
-			echo '<div class="control-group">
-					<label class="control-label" for="commentaire">Votre commentaire :</label>
-					<div class="controls">
-						<textarea name="commentaire" id="commentaire" placeholder="Votre commentaire" rows="3" class="span6">';
-			//si il y a un commentaire en session on prends		
-			if(isset($_SESSION['commentaire'])){
-				echo $_SESSION['commentaire'];
-			}
-			echo		'</textarea>
-					</div>
-				</div>';
-			
-			//reCaptcha
-			require_once('libraries/recaptchalib.php');
-			include('functions/InstallInfo.php');
-			
-			$error = null;
-			echo '<div class="control-group">
-					<label class="control-label" for=""></label>
-					<div class="controls">
-						'.recaptcha_get_html($publicKeyReCaptcha, $error).'
-					</div>
-				</div>';
+				echo '</section>';
 				
-			echo '<br/><div class="control-group">
-					<div class="controls">
-						<button type="submit" class="btn">Envoyer !</button>
-					</div>
-				</div>';
-			echo '</form>';
-			
-			echo '</div></div>';			
-			echo '<div class="span2"></div></div></section>';
-			
-			//si le mec est pas logué, on détruit la session
-			if(!$isAdmin){
-				session_destroy();
-				$_SESSION = array();
-			}else{
-				$_SESSION['nomCommentateur']="";
-				$_SESSION['commentaire']="";
+				//partie insertion de commentaire
+				echo '<section id="addCom"><br/><div class="row"><div class="span2"></div>';
+				echo '<div class="span8"><div class="well well-small">';
+				echo '<i class="icon-comment"></i> <small>Ajouter un commentaire</small>';
+				
+				echo '<form class="form-horizontal" method="post" action="addComment.php?art='.$_GET['art'].'">';
+				echo '<div class="control-group">
+						<label class="control-label" for="commentateur">Votre nom :</label>
+						<div class="controls">
+							<input type="text" name="commentateur" id="commentateur" placeholder="Votre nom" value="';
+				//si il y a un nom en session on prends
+				if(isset($_SESSION['nomCommentateur'])){
+					echo $_SESSION['nomCommentateur'];
+				}
+				echo		'">
+						</div>
+					</div>';
+							
+				echo '<div class="control-group">
+						<label class="control-label" for="commentaire">Votre commentaire :</label>
+						<div class="controls">
+							<textarea name="commentaire" id="commentaire" placeholder="Votre commentaire" rows="3" class="span6">';
+				//si il y a un commentaire en session on prends		
+				if(isset($_SESSION['commentaire'])){
+					echo $_SESSION['commentaire'];
+				}
+				echo		'</textarea>
+						</div>
+					</div>';
+				
+				//reCaptcha
+				require_once('libraries/recaptchalib.php');
+				include('functions/InstallInfo.php');
+				
+				$error = null;
+				echo '<div class="control-group">
+						<label class="control-label" for=""></label>
+						<div class="controls">
+							'.recaptcha_get_html($publicKeyReCaptcha, $error).'
+						</div>
+					</div>';
+					
+				echo '<br/><div class="control-group">
+						<div class="controls">
+							<button type="submit" class="btn">Envoyer !</button>
+						</div>
+					</div>';
+				echo '</form>';
+				
+				echo '</div></div>';			
+				echo '<div class="span2"></div></div></section>';
+				
+				//si le mec est pas logué, on détruit la session
+				if(!$isAdmin){
+					session_destroy();
+					$_SESSION = array();
+				}else{
+					$_SESSION['nomCommentateur']="";
+					$_SESSION['commentaire']="";
+				}
 			}
 			
 			if(isset($_GET['message']) && $_GET['message']!=''){
